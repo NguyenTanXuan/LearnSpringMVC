07cb1be292fd3807aa55df276fa653329a9773da
NguyenTanXuan <tanxuan31052004@gmail.com>
2025-05-21 21:03:50 +0700

    add plus controller

--- src/main/java/com/tanxuan/springmvc/GreetingController.java
+++ src/main/java/com/tanxuan/springmvc/GreetingController.java
@@ -3,12 +3,16 @@ package com.tanxuan.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-        @GetMapping("/hello-xuan")
-        public String greet(Model model) {
-            model.addAttribute("name", "Xuan");
+        @GetMapping("/plus")
+        public String plus(Model model, @RequestParam(name = "first", defaultValue = "5") int a,@RequestParam(name ="second", defaultValue = "7") int b) {
+            int result = a + b;
+            model.addAttribute("first", a);
+            model.addAttribute("second", b);
+            model.addAttribute("result", result);
             return "greeting";
         }
 }
